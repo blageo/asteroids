f79fd8553321460c873f0ae64e531fe5244a383d
blageo <blageo@proton.me>
2025-03-03 23:55:29 -0500

    implemented delta time

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ from constants import *
 
 def main():
     pygame.init()
+
+    # Setup screen
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    
+    # Welcome messages
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # Clock
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -14,6 +22,9 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+        print(dt)
 
 
 if __name__ == "__main__":
